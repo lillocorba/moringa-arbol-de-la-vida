8b3efdd5170893c04fd58585021784c214f7d457
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2021-06-16 23:16:05 -0300

    Animacion menu en proceso

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".contenedor__burger")
+const line1 = document.querySelector(".line1")
+const line2 = document.querySelector(".line2")
+const line3 = document.querySelector(".line3")
+const menu = document.querySelector(".contenedor__menu__ul")
+
+burger.addEventListener("click", () => {
+    line1.classList.toggle('line1-active')
+    line2.classList.toggle('line2-active')
+    line3.classList.toggle('line3-active')
+    menu.classList.toggle('contenedor__menu__ul-active')
+})
