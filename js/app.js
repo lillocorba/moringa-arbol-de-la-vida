e02c3911d2d95456f2cde2b56e3cd6a9158758fc
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2021-06-16 23:37:00 -0300

    Animaciones Menu OK

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const menu = document.querySelector(".contenedor__menu__ul")
 const linkMenu1 = document.querySelector(".link-menu-1")
 const linkMenu2 = document.querySelector(".link-menu-2")
 const linkMenu3 = document.querySelector(".link-menu-3")
+const linksMenu = document.querySelectorAll(".link-menu")
 
 burger.addEventListener("click", () => {
     line1.classList.toggle('line1-active')
@@ -15,4 +16,16 @@ burger.addEventListener("click", () => {
     linkMenu1.classList.toggle('link-menu-1-active')
     linkMenu2.classList.toggle('link-menu-2-active')
     linkMenu3.classList.toggle('link-menu-3-active')
+})
+
+linksMenu.forEach((link) => {
+    link.addEventListener("click", () => {
+        menu.classList.toggle('contenedor__menu__ul-active')
+        line1.classList.toggle('line1-active')
+        line2.classList.toggle('line2-active')
+        line3.classList.toggle('line3-active')
+        linkMenu1.classList.toggle('link-menu-1-active')
+        linkMenu2.classList.toggle('link-menu-2-active')
+        linkMenu3.classList.toggle('link-menu-3-active')
+    })
 })
